6ee848259b488b5106626a6f8c5b1ecbb03bafa8
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 08:16:22 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 const _state = {
 	isOnline: false,
 	userName: 'Harald Lingren',
-	daysTillReportIsDue: 1
+	daysTillReportIsDue: 1,
 };
 
 function App() {
@@ -13,36 +13,47 @@ function App() {
 	const handleToggleLogin = () => {
 		setState({
 			...state,
-			isOnline: !state.isOnline
+			isOnline: !state.isOnline,
 		});
 	};
 
 	const handleChangeDaysDue = () => {
 		setState({
 			...state,
-			daysTillReportIsDue: state.daysTillReportIsDue + 1
+			daysTillReportIsDue: state.daysTillReportIsDue + 1,
 		});
-	}
-	
+	};
+
+	const getMessageStyle = () => {
+		return {
+			fontSize: `${1 + state.daysTillReportIsDue / 10}rem`,
+			color: state.daysTillReportIsDue < 5 ? 'orange' : 'red',
+			backgroundColor: state.daysTillReportIsDue < 8 ? '' : 'tomato', 
+		};
+	};
+
 	return (
 		<div className="App">
 			<h1>Info Site</h1>
 			<p>
 				<span
-					className="userName"
-					style={{
-						backgroundColor: state.isOnline ? 'green' : '#444',
-					}}
+					className={`userName ${state.isOnline ? 'loggedIn' : 'loggedOut'}`}
 				>
 					{state.userName}
 				</span>
 			</p>
-			<p className="message" style={{fontSize: `${1 + (state.daysTillReportIsDue/10)}rem`, color: state.daysTillReportIsDue < 5 ? "orange" : "red"}}>Report is due in {state.daysTillReportIsDue} days.</p>
+			<p className="message" style={getMessageStyle()}>
+				Report is due in {state.daysTillReportIsDue} days.
+			</p>
 			<hr />
 			<section className="emulation">
 				<h2>Emulate State Change</h2>
-				<button onClick={() => handleToggleLogin()}>toggle logged in</button>
-				<button onClick={() => handleChangeDaysDue()}>change days due</button>
+				<button onClick={() => handleToggleLogin()}>
+					toggle logged in
+				</button>
+				<button onClick={() => handleChangeDaysDue()}>
+					change days due
+				</button>
 			</section>
 		</div>
 	);
