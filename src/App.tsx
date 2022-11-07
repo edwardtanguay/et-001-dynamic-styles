faebcff91c6dacfdeb47aec882ec35a732e3a270
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-07 07:53:18 +0100

    state simple

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 const _state = {
 	isOnline: false,
 	userName: 'Harald Lingren',
-	daysTillReportIsDue: 4
+	daysTillReportIsDue: 1
 };
 
 function App() {
@@ -17,6 +17,13 @@ function App() {
 		});
 	};
 
+	const handleChangeDaysDue = () => {
+		setState({
+			...state,
+			daysTillReportIsDue: state.daysTillReportIsDue + 1
+		});
+	}
+	
 	return (
 		<div className="App">
 			<h1>Info Site</h1>
@@ -30,10 +37,12 @@ function App() {
 					{state.userName}
 				</span>
 			</p>
+			<p className="message" style={{fontSize: `${1 + (state.daysTillReportIsDue/10)}rem`, color: state.daysTillReportIsDue < 5 ? "orange" : "red"}}>Report is due in {state.daysTillReportIsDue} days.</p>
 			<hr />
 			<section className="emulation">
 				<h2>Emulate State Change</h2>
 				<button onClick={() => handleToggleLogin()}>toggle logged in</button>
+				<button onClick={() => handleChangeDaysDue()}>change days due</button>
 			</section>
 		</div>
 	);
